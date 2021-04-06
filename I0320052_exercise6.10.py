52131ad96798aa5ea72a56d7cc24bbc9bf0e29b1
ilhammaulana28 <80144779+ilhammaulana28@users.noreply.github.com>
2021-04-07 00:23:29 +0700

    Tugas 6 Praktik Programa Komputer

---
+++ I0320052_exercise6.10.py
@@ -0,0 +1,9 @@
+print('Exercise 6.10')
+i = 1
+while i < 10:
+    j = 1
+    while j < i:
+        print('%d' % (i*j), end='')
+        j = j + 1
+    print()
+    i = i + 1
---
+++ I0320052_exercise6.11.py
@@ -0,0 +1,5 @@
+print('Exercise 6.11')
+for i in range(11):
+    print(i, end='')
+    if i == 7:
+        break
